88ee71ecdfc7dcd166a474cfd60af5136a03f118
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-11-08 11:58:16 -0600

    first commit

---
+++ src/LoginPage.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class LoginPage implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("userID:");
+    JLabel userPasswordLabel = new JLabel("password:");
+    JLabel messageLabel = new JLabel();
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+    LoginPage(HashMap<String, String> loginInfoOriginal) {
+    logininfo =loginInfoOriginal;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
